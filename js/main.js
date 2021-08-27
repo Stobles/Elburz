53aa544a07f93e5bcb476042dd578a04dba1c485
Stobles <82175280+Stobles@users.noreply.github.com>
2021-08-27 19:57:40 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-console.time('Работает')
-
 const headerLogo = document.querySelector('.header__logo')
 window.addEventListener('load', ()=>{
     const widthOfWindow = window.innerWidth
@@ -20,30 +18,42 @@ window.addEventListener('resize', ()=>{
     }
 })
 
-const animBlock = document.querySelector('.innovate')
-let animBlockHeightFixed = animBlock.clientHeight
-let animBlockHeightAction
-
-
+const innovate = document.querySelector('.innovate')
+const innovateH = innovate.clientHeight
 
 window.addEventListener('scroll', ()=>{
-    let animBlockHeightAction = animBlock.clientHeight
-    window.addEventListener('load', ()=>{
-        let animBlockOffsetTop = animBlock.getBoundingClientRect().top;
-        let animBlockHeightScroll = animBlockHeightFixed + (animBlockOffsetTop -
-        250) - 200;
-            
-        animBlock.style.height = animBlockHeightScroll +'px'
-    })
+    if(!(innovateH - window.scrollY/2 + 100 < 0)){
+        console.log(innovateH - window.scrollY/2 + 100)
+        innovate.style.height = innovateH - window.scrollY/1.6 + 100 + 'px'
         
-    window.addEventListener('scroll', ()=>{
-        let animBlockOffsetTop = animBlock.getBoundingClientRect().top;
-        let animBlockHeightScroll = animBlockHeightFixed + (animBlockOffsetTop - 250) - 200;
-            
-        animBlock.style.height = animBlockHeightScroll +'px'
-
-    })
+    }
 })
+
+
+// const animBlock = document.querySelector('.innovate')
+// let animBlockHeightFixed = animBlock.clientHeight
+// let animBlockHeightAction
+
+
+
+// window.addEventListener('scroll', ()=>{
+//     window.addEventListener('load', ()=>{
+//         let animBlockOffsetTop = animBlock.getBoundingClientRect().top;
+//         let animBlockHeightScroll = animBlockHeightFixed + (animBlockOffsetTop -
+//         250) - 200;
+            
+//         animBlock.style.height = animBlockHeightScroll +'px'
+//     })
+
+//     window.addEventListener('resize', ()=>{
+//         let animBlockOffsetTop = animBlock.getBoundingClientRect().top;
+//         let animBlockHeightScroll = animBlockHeightFixed + (animBlockOffsetTop -
+//             250) - 200;
+            
+//         animBlock.style.height = animBlockHeightScroll +'px'
+
+//     })
+// })
 
 const headerWrapper = document.querySelector('.header__wrapper')
 const burger = document.querySelector('.header__burger')
@@ -117,9 +127,148 @@ gsap.to(".featured__body--item--size-right", {
     y: 0,
     opacity: 1,
     duration: 1,
+    delay: .4
+})
+
+gsap.to(".achievements__info--title", {
+    scrollTrigger: {
+        trigger: ".achievements__info--title",
+        start: '-1500'
+    },
+    x: 0,
+    opacity: 1,
+    duration: 1,
+})
+
+gsap.to(".achievements__info--block--main", {
+    scrollTrigger: {
+        trigger: ".achievements__info--block--main",
+        start: '-1500',
+    },
+    opacity: 1,
+    duration: 1,
+})
+
+gsap.to(".achievements__body--block--size-all", {
+    scrollTrigger: {
+        trigger: ".achievements__body--block--size-all",
+        start: '-1460',
+    },
+    opacity: 1,
+    duration: 1,
+})
+
+gsap.to(".achievements__body--block--size-left", {
+    scrollTrigger: {
+        trigger: ".achievements__body--block--size-left",
+        start: '-1500',
+    },
+    opacity: 1,
+    duration: 1,
+})
+
+gsap.to(".achievements__body--block--size-right", {
+    scrollTrigger: {
+        trigger: ".achievements__body--block--size-right",
+        start: '-1500',
+    },
+    opacity: 1,
+    duration: 1,
     delay: .4,
 })
 
+gsap.to(".inquire__title", {
+    scrollTrigger: {
+        trigger: ".inquire__title",
+        start: '-1200'
+    },
+    x: 200,
+    opacity: 1,
+    duration: 1,
+})
 
-console.timeEnd('Работает')
+gsap.to(".inquire__body--content", {
+    scrollTrigger: {
+        trigger: ".inquire__body--content",
+        start: '-1400'
+    },
+    opacity: 1,
+    duration: 1,
+})
+
+// Скрипт анимации 
+
+
+const animItems = document.querySelectorAll('.anim__items');
+
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (var i = 0; i < animItems.length; i++) {
+            const animItem = animItems[i];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 1;
+
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = (window.innerHeight - window.innerHeight / animStart);
+
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('active')
+            }
+            else {
+                if (!animItem.classList.contains('anim-no-hide')) {
+                    animItem.classList.remove('acitve');
+                }
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+
+    }
+
+    setTimeout(animOnScroll, 400);
+}
+
+
+// Анимация Счетчика //
+
+const endNumber = document.querySelector('#end-number')
+const time = 2000;
+const step = 1;
+var outNumOneTime = false
+
+window.addEventListener('scroll', ()=>{
+    if(document.querySelector('.achievements__info--block--minor').classList.contains('active')){
+        if(!outNumOneTime){
+            outNum(endNumber.innerHTML, '#number')
+        }
+    }
+})
+
+function outNum(num, elem) {
+    let e = document.querySelector(elem);
+    n = 0;
+    let t = Math.round(time/(num/step));
+    let interval = setInterval(() => {
+        n = n + step;
+        if(n == num) {
+            clearInterval(interval);
+        }
+        e.innerHTML = n;
+    },t);
+    outNumOneTime = true
+};
 
